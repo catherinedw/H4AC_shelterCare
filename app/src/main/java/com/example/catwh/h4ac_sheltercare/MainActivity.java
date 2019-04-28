81f109fb888fa29ac15d1e02cc9cdb87d9f05543
catherinedw <catwhitaker@gmail.com>
2019-04-27 23:47:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/catwh/h4ac_sheltercare/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.catwh.h4ac_sheltercare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://hfac-sc.firebaseapp.com/");
+
+    }
+}
